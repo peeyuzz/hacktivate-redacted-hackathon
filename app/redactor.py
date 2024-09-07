a92ae12a887f34dc5b734abea5a35834d8a5ffc6
Utsav Singh <singhutsavofficial@gmail.com>
2024-09-07 23:52:35 +0530

    add support for linux environment

--- app/redactor.py
+++ app/redactor.py
@@ -14,14 +14,18 @@ import cv2
 from openvino.runtime import Core
 import numpy as np
 from app import utils
+import platform
 
 load_dotenv()
 
 API_KEY = os.environ["API_KEY"]
 genai.configure(api_key=API_KEY)
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
-
-
+# Define tesseract command path based on the OS
+if platform.system() == "Windows":
+    pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
+elif platform.system() == "Linux":
+    # Typically Tesseract is installed in /usr/bin/tesseract on Linux
+    pytesseract.pytesseract.tesseract_cmd = '/usr/bin/tesseract'
 class FaceDetector:
     def __init__(self,
                  model,
@@ -39,7 +43,7 @@ class FaceDetector:
         # In our case here, is a output for hte bbox and other for the score
         # /confidence. Have a look at the openvino documentation for more i
         self.output_scores_layer = self.model.output(0)
-        self.output_boxes_layer  = self.model.output(1)
+        self.output_boxes_layer = self.model.output(1)
         # confidence threshold
         self.confidence_thr = confidence_thr
         # threshold for the nonmaximum suppression
@@ -51,22 +55,24 @@ class FaceDetector:
             in the BGR format of any shape.
         """
         # resize to match the expected by the model
-        input_image = cv2.resize(image, dsize=[320,240])
+        input_image = cv2.resize(image, dsize=[320, 240])
         # changing from [H, W, C] to [C, H, W]. "channels first"
-        input_image = np.expand_dims(input_image.transpose(2,0,1), axis=0)
+        input_image = np.expand_dims(input_image.transpose(2, 0, 1), axis=0)
         return input_image
 
     def posprocess(self, pred_scores, pred_boxes, image_shape):
         # get all predictions with more than confidence_thr of confidence
-        filtered_indexes = np.argwhere( pred_scores[0,:,1] > self.confidence_thr  ).tolist()
-        filtered_boxes   = pred_boxes[0,filtered_indexes,:]
-        filtered_scores  = pred_scores[0,filtered_indexes,1]
+        filtered_indexes = np.argwhere(
+            pred_scores[0, :, 1] > self.confidence_thr).tolist()
+        filtered_boxes = pred_boxes[0, filtered_indexes, :]
+        filtered_scores = pred_scores[0, filtered_indexes, 1]
 
         if len(filtered_scores) == 0:
-            return [],[]
+            return [], []
 
         # convert all boxes to image coordinates
         h, w = image_shape
+
         def _convert_bbox_format(*args):
             bbox = args[0]
             x_min, y_min, x_max, y_max = bbox
@@ -76,48 +82,51 @@ class FaceDetector:
             y_max = int(h*y_max)
             return x_min, y_min, x_max, y_max
 
-        bboxes_image_coord = np.apply_along_axis(_convert_bbox_format, axis = 2, arr=filtered_boxes)
+        bboxes_image_coord = np.apply_along_axis(
+            _convert_bbox_format, axis=2, arr=filtered_boxes)
 
         # apply non-maximum supressions
-        bboxes_image_coord, indexes = utils.non_max_suppression(bboxes_image_coord.reshape([-1,4]),
+        bboxes_image_coord, indexes = utils.non_max_suppression(bboxes_image_coord.reshape([-1, 4]),
                                                                 overlapThresh=self.overlap_thr)
         filtered_scores = filtered_scores[indexes]
         return bboxes_image_coord, filtered_scores
 
-    def draw_bboxes(self, image, bboxes, color=[0,255,0]):
+    def draw_bboxes(self, image, bboxes, color=[0, 255, 0]):
         # Just for visualization
         # draw all bboxes on the input image
         for boxe in bboxes:
             x_min, y_min, x_max, y_max = boxe
             pt1 = (x_min, y_min)
             pt2 = (x_max, y_max)
-            cv2.rectangle(image, pt1, pt2, color=color, thickness=2, lineType=cv2.LINE_4)#BGR
+            cv2.rectangle(image, pt1, pt2, color=color,
+                          thickness=2, lineType=cv2.LINE_4)  # BGR
 
     def inference(self, image):
         input_image = self.preprocess(image)
         # inference
-        pred_scores = self.model( [input_image] )[self.output_scores_layer]
-        pred_boxes = self.model( [input_image] )[self.output_boxes_layer]
+        pred_scores = self.model([input_image])[self.output_scores_layer]
+        pred_boxes = self.model([input_image])[self.output_boxes_layer]
 
         image_shape = image.shape[:2]
         faces, scores = self.posprocess(pred_scores, pred_boxes, image_shape)
         return faces, scores
-    
+
+
 class Redactor:
     # Class variables for regex patterns
     EMAIL_PATTERN = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b'
     PHONE_PATTERN = r'\b(?:\+?\d{1,4}[\s-]?)?(?!0+\b)\d{10}\b'
     AADHAR_PATTERN = r'[2-9]\d{3}\s?-?\d{4}\s?-?\d{4}'
-    PASSPORT_PATTERN = r'\b[A-PR-WY][1-9]\d{7}\b'  
+    PASSPORT_PATTERN = r'\b[A-PR-WY][1-9]\d{7}\b'
     PAN_PATTERN = r'\b[A-Z]{5}[0-9]{4}[A-Z]\b'
     BANK_ACC_PATTERN = r'\b\d{9,18}\b'
-
-    def __init__(self, path, plan_type = "free"):
+    def __init__(self, path, plan_type="free"):
+        model_path = os.path.join("model", "public", "ultra-lightweight-face-detection-rfb-320", "FP16", "ultra-lightweight-face-detection-rfb-320.xml")
         self.path = path
         self.texts = ""
         self.plan_type = plan_type
         self.sensitive_datas = []
-        self.face_detector = FaceDetector(model=r"model\public\ultra-lightweight-face-detection-rfb-320\FP16\ultra-lightweight-face-detection-rfb-320.xml")
+        self.face_detector = FaceDetector(model=model_path)
 
     
     def get_sensitive_data(self, text):
