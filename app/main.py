3ab8900a8cbd61d11b79f9c6c603f898e9be7f96
abhinavcool42 <abhinavcool42@gmail.com>
2024-09-08 16:12:27 +0530

    e

--- app/main.py
+++ app/main.py
@@ -246,4 +246,4 @@ async def get_user_files(current_user: dict = Depends(get_current_user)):
        
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
